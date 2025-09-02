de1bed9f42b844fb479df9793f9cf5ee4d197658
Renan Nandi <renannandi@gmail.com>
2025-09-02 11:44:54 -0300

    First commit

---
+++ src/test/java/com/senai/lab365/exercicio_m3s1/ExercicioM3s1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.lab365.exercicio_m3s1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioM3s1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
